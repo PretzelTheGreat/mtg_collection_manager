ef7e5d18529dbbc032b66d8a8e79d1eb7bdf2284
Christian Wetzel <christian.m.wetzel@gmail.com>
2023-07-09 14:25:20 -0400

    added more functionality for logging

--- src/util_funcs.py
+++ src/util_funcs.py
@@ -5,30 +5,33 @@ import csv
 import zipfile
 
 show_log = True
+DEBUG_LEVEL = "DEBUG"
+
+level_mapping = {"INFO": 0, "DEBUG": 1, "WARNING": 2, "ERROR": 3, "CRITICAL": 4}
 
 def log_message(message_string, level):
-    if show_log:
+    if show_log and level_mapping[level] >= level_mapping[DEBUG_LEVEL]:
         print(f"{level} - {message_string}")
 
 def import_json_file(filename):
     data = {}
-    log_message(f"loading {filename}", "DEBUG")
+    log_message(f"loading {filename}", "INFO")
     with open(filename, 'r', encoding="utf-8") as open_file:
         data = json.load(open_file)
 
-    log_message(f"finished loading {filename}", "DEBUG")
+    log_message(f"finished loading {filename}", "INFO")
     return data
 
 def import_csv_file(filename):
     data = []
-    log_message(f"loading {filename}", "DEBUG")
+    log_message(f"loading {filename}", "INFO")
     with open(filename, 'r') as open_file:
         csv_reader = csv.DictReader(open_file)
 
         for line in csv_reader:
             data.append(line)
 
-    log_message(f"finished loading {filename}", "DEBUG")
+    log_message(f"finished loading {filename}", "INFO")
     return data
 
 def export_json_file(filename, data, indent=4):
@@ -52,21 +55,21 @@ def download_file_from_url(url, filename):
     # first create a temporary directory to hold the download file
     create_tmp_dir()
 
-    log_message(f"requesting resources from url: '{url}'", "DEBUG")
+    log_message(f"requesting resources from url: '{url}'", "INFO")
     # next, process the request
     r = requests.get(url, allow_redirects=True, timeout=0.500)
-    log_message(f"received request data", "DEBUG")
+    log_message(f"received request data", "INFO")
     
-    log_message(f"writing contets of request to '{filename}'", "DEBUG")
+    log_message(f"writing contets of request to '{filename}'", "INFO")
     # write the file with the desired filename (should be roughly what the end filename should be)
     with open(f"resources/tmp/{filename}", 'wb') as open_file:
         open_file.write(r.content)
-    log_message(f"done writing request contents", "DEBUG")
+    log_message(f"done writing request contents", "INFO")
 
 
 def unzip_file_to_loc(filepath, target_loc):
     # filepath should be passed here, not just the filename
-    log_message(f"unzipping '{filepath}' to '{target_loc}'", "DEBUG")
+    log_message(f"unzipping '{filepath}' to '{target_loc}'", "INFO")
     with zipfile.ZipFile(filepath, 'r') as zip_ref:
         zip_ref.extractall(target_loc)
-    log_message(f"done unzipping {filepath}", "DEBUG")
+    log_message(f"done unzipping {filepath}", "INFO")
