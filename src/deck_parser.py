10767dc96a6b9ecce8b824623e72cb91b0626245
Christian Wetzel <christian.m.wetzel@gmail.com>
2023-07-22 09:59:17 -0400

    changed how basic lands are handled

--- src/deck_parser.py
+++ src/deck_parser.py
@@ -2,10 +2,16 @@ import src.pricing_info as pricing_info
 
 def get_deck_stats(deck, pricing_data=None):
     deck_stats = {"commander": 1}
+    basic_lands = {}
 
     for k, v in deck["the_99"].items():
         if  k == "basic_lands":
-            deck_stats["basic_lands"] = sum(v.values())
+            for card in v:
+                if card["name"] not in basic_lands.keys():
+                    basic_lands[card["name"]] = 1
+                else:
+                    basic_lands[card["name"]] += 1
+            deck_stats["basic_lands"] = sum(basic_lands.values())
 
         elif len(v) != 0:
             deck_stats[k] = len(v)
