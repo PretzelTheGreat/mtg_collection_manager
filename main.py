27e35c1eb42f813d706238f1d79c9450ea093498
Christian Wetzel <christian.m.wetzel@gmail.com>
2024-04-25 01:23:17 -0400

    using new database as collection

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ import src.collection_manager as collection_manager
 # print("Exported pricing data for collection")
 
 # OTJ rares
-collection_filename = "resources/databases/OTJ_rares.json"
-otj_rares = collection_manager.CollectionManager(collection_filename, skip_decks=True, debug_level="ERROR")
-value_at_100 = otj_rares.calculate_value_of_collection()
-print(f"Value of OTJ rares ($2 minimum): {value_at_100}\nHighest value card: {otj_rares.highest_value_card}")
-otj_rares.export_pricing_data()
+collection_filename = "resources/databases/my_collection_2024.json"
+my_collection = collection_manager.CollectionManager(collection_filename, skip_decks=True, debug_level="ERROR")
+value_at_100 = my_collection.calculate_value_of_collection()
+
+my_collection.export_pricing_data(include_in_use=True)
